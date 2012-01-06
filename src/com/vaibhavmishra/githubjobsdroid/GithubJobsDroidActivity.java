37375e03f3c501dcbbf0fa8f6ff6f81a8f1cfb53
Vaibhav Mishra <vinu76jsr@gmail.com>
2012-01-06 07:38:55 +0530

    initial directory structure
    Initial directory structure with async http library in github folder

---
+++ src/com/vaibhavmishra/githubjobsdroid/GithubJobsDroidActivity.java
@@ -0,0 +1,14 @@
+package com.vaibhavmishra.githubjobsdroid;
+import com.loopj.android.http.*;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GithubJobsDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
