1bf0e6a3ff727d4263b665b0efa05490c4b53ca5
Vaibhav Mishra <vinu76jsr@gmail.com>
2012-01-11 23:35:47 +0530

    manifests etc.,

--- gen/com/vaibhavmishra/githubjobsdroid/R.java
+++ gen/com/vaibhavmishra/githubjobsdroid/R.java
@@ -10,14 +10,75 @@ package com.vaibhavmishra.githubjobsdroid;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int app_background_dark_yellow=0x7f04000b;
+        public static final int app_background_lite=0x7f04000a;
+        public static final int black=0x7f04001d;
+        public static final int blue=0x7f040002;
+        public static final int chapter_list_header_gradient_bottom=0x7f040016;
+        public static final int chapter_list_header_gradient_top=0x7f040015;
+        public static final int course_list_item_background=0x7f040021;
+        public static final int course_list_item_background_selected=0x7f040022;
+        public static final int divider_yellow=0x7f04000c;
+        public static final int grey=0x7f04001c;
+        public static final int inactive_text=0x7f04000f;
+        public static final int list_background_grey_divider=0x7f040019;
+        public static final int list_background_selected=0x7f040008;
+        public static final int list_background_unselected=0x7f040009;
+        public static final int login_screen_login_button=0x7f040005;
+        public static final int login_screen_login_button_text=0x7f040003;
+        public static final int login_screen_login_button_text_disabled=0x7f040004;
+        public static final int ombook_viewer_footer_gradient_bottom=0x7f040013;
+        public static final int ombook_viewer_footer_gradient_top=0x7f040012;
+        public static final int ombook_viewer_header_gradient_bottom=0x7f040011;
+        public static final int ombook_viewer_header_gradient_top=0x7f040010;
+        public static final int ombook_viewer_header_strip_tab_bar_background=0x7f04001f;
+        public static final int ombook_viewer_landing_background=0x7f040014;
+        public static final int pack_header_text_color=0x7f040023;
+        public static final int packages_title_element_gradient_bottom=0x7f04001b;
+        public static final int packages_title_element_gradient_top=0x7f04001a;
+        public static final int page_list_header_gradient_bottom=0x7f040018;
+        public static final int page_list_header_gradient_top=0x7f040017;
+        public static final int settings_logout_button_text=0x7f040020;
+        public static final int sirius_black=0x7f040000;
+        public static final int tab_selected=0x7f040006;
+        public static final int tab_unselected=0x7f040007;
+        public static final int theme_purple=0x7f04001e;
+        public static final int white=0x7f040001;
+        public static final int white_gray=0x7f040024;
+        public static final int yellow_lite=0x7f04000e;
+        public static final int yellow_normal=0x7f04000d;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrow_next=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int job_item_arrow=0x7f060003;
+        public static final int job_item_company_fulltime=0x7f060002;
+        public static final int job_item_location=0x7f060005;
+        public static final int job_item_position=0x7f060001;
+        public static final int job_item_time=0x7f060004;
+        public static final int listView1=0x7f060006;
+        public static final int main_description=0x7f060009;
+        public static final int main_description_label=0x7f060008;
+        public static final int main_fulltime=0x7f06000d;
+        public static final int main_fulltime_label=0x7f06000c;
+        public static final int main_location=0x7f06000b;
+        public static final int main_location_label=0x7f06000a;
+        public static final int main_progressBar1=0x7f06000f;
+        public static final int main_root=0x7f060007;
+        public static final int main_submit=0x7f06000e;
+        public static final int webView1=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int job_description=0x7f030000;
+        public static final int job_item=0x7f030001;
+        public static final int joblist=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
--- src/com/vaibhavmishra/githubjobsdroid/JobDescription.java
+++ src/com/vaibhavmishra/githubjobsdroid/JobDescription.java
@@ -1,23 +1,65 @@
 package com.vaibhavmishra.githubjobsdroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class JobDescription extends Activity {
+	
+	private JobDescription ctx;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.job_description);
+		
+		ctx = this;
 
 		int pos = getIntent().getIntExtra("pos", 0);
 
 		WebView webview = (WebView) findViewById(R.id.webView1);
+		WebSettings websettings = webview.getSettings();
+		websettings.setJavaScriptEnabled(true);
+		
 		Position data = Data.positions[pos];
 
 		String html = "<b><a href=" + data.company_url + ">" + data.company
 				+ "</a></b><br/>" + data.description
 				+ "<br/><b>How to Apply</b><br/>" + data.how_to_apply + "<br/>";
 		webview.loadData(html, "text/html", "UTF-8");
+	    webview.setWebViewClient( new YourWebClient()); //TODO later for setting mailto: 
 
 	}
+	
+	private class YourWebClient extends WebViewClient {     
+	    @Override
+	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	        if (url.contains("mailto:")) {
+	        	String emailAddress[] = url.split(":");
+	            // TODO: extract the email... that's your work, LOL
+	            String email = emailAddress[1];
+	            sendEmail(email);
+	            return super.shouldOverrideUrlLoading(view, url);
+	        }
+	        view.loadUrl(url);
+	        return true;
+	    }
+	    
+	    public void sendEmail(String email){
+		    final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		    emailIntent.setType("plain/text");
+		    emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{email});
+
+		    String mySubject = "this is just if you want";
+		    emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, mySubject="");
+		    String myBodyText = "this is just if you want";
+		    emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, myBodyText="");
+		    ctx.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+		}
+	}
+	
+	
+
 }
